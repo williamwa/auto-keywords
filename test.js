f8137034daa5a5b8bf7de6d5fca4a145a0479cc6
djhell <daniele@brugnara.me>
2014-06-07 11:27:38 +0200

    added dictionaries and optionalized parameters

--- test.js
+++ test.js
@@ -1,27 +1,10 @@
-Extract keywords from text
+var extract = require('./index');
 
-to use it:
-
-```
-npm install auto-keywords
-```
-
-```
-var extract = require('auto-keywords');
-
-var keywords = extract('your text here');
-```
-
-## Options
-
-You can pass some options to `extract()` this way:
-
-```
 var options = {
   limit: 100, // defaults to 10
   wordMinLen: 2, // words min len to be counted, defaults to 3
   completeReturns: true, // defaults to false. Thi will return word count instead of only unique words
   language: 'all' // defaults to 'en'. Putting all, we will use all dictionaries got from **dicts folder**
 };
-var keywords = extract('your text here', options);
-```
+
+console.log(extract('hey buddy, what\'s up!', options));
